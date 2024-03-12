219dbfa607a7baf09e2f2ef68ae69b79596dccd2
Filovale <vfilippo977@gmail.com>
2024-03-12 22:20:38 +0100

    boo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Main from './components/main/main.jsx';
 import Footer from './components/Footer/Footer.jsx';
-import myNavbar from './components/myNavbar/myNavbar.jsx';
+
 
 function App() {
   return (
     <>
-      <myNavbar />
       <Main />
       <Footer />
     </>
