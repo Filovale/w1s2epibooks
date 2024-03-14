accf35fd2f468719e2d82a935970f0594f60da99
Filovale <vfilippo977@gmail.com>
2024-03-14 16:47:43 +0100

    final w1d2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Main from './components/main/main.jsx';
 import Footer from './components/Footer/Footer.jsx';
+import Navbar from './components/Navbar/Navbar.jsx';
 
 
 function App() {
   return (
     <>
+      <Navbar />    
       <Main />
       <Footer />
     </>
