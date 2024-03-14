ae04dd2ab494ac07707fa68791880147d0f07d87
Filovale <vfilippo977@gmail.com>
2024-03-14 16:55:01 +0100

    final w1d2

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Main from './components/main/main.jsx';
 import Footer from './components/Footer/Footer.jsx';
 import Navbar from './components/Navbar/Navbar.jsx';
+import myAlert from './components/myAlert/myAlert.jsx';
 
 
 function App() {
   return (
     <>
+      <myAlert />
       <Navbar />    
       <Main />
       <Footer />
